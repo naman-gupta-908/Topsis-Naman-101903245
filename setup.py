e62a58086537eba386bc905feead7708ceeb73aa
naman-gupta-908 <57359890+naman-gupta-908@users.noreply.github.com>
2022-02-26 16:34:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,7 @@ setup(
   download_url = 'https://github.com/naman-gupta-908/Topsis-Naman-101903245/archive/refs/tags/1.0.tar.gz',    # I explain this later on
   keywords = ['TOPSIS', 'WEIGHTS', 'IMPACTS','RANKS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'pandas',
-          'math',
-          'sys'
+          'pandas'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
